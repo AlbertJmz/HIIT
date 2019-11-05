06b8abfe7c22c01107752f08f45cd8c7abf8a7b3
AlbertJmz <55957539+JorgenTech@users.noreply.github.com>
2019-11-05 11:03:59 -0600

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HIIT';
+  subtitle= 'Acceso a clientes';
+  fb='Facebook';
+  tw='Twitter';
+  a='Acerca de Nosotros';
+  b='contacto@hiit.com';
+  c='Aviso de Privacidad';
+  d='Domina las metricas que importan y toma';
+  e='mejores desiciones con nuestras';
+  f='herramientas de monitoreo en tiempo real';
+}
